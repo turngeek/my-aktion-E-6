3fcac65753be005f4473d55105104f6932f11c33
Martin Schmollinger <martin.schmollinger@gmail.com>
2020-02-05 16:21:09 +0100

    Added REST resource class for campaign service

---
+++ campaignservice/src/main/java/press/turngeek/myaktion/api/CampaignResource.java
@@ -0,0 +1,65 @@
+package press.turngeek.myaktion.api;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import press.turngeek.myaktion.boundary.CampaignService;
+import press.turngeek.myaktion.model.Campaign;
+
+/**
+ * CampaignResource
+ * 
+ */
+@Path("/campaign")
+public class CampaignResource {
+
+    @Inject
+    private CampaignService campaignService;
+
+    @GET
+    @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Campaign> getAllCampaigns() {
+        List<Campaign> allCampaigns = campaignService.getCampaigns();
+        allCampaigns.forEach(campaign -> {
+            campaign.setDonations(null);
+        });
+        return allCampaigns;
+    }
+
+    @DELETE
+    @Path("/{campaignId}")
+    public void deleteCampaign(@PathParam(value = "campaignId") Long campaignId) {
+        campaignService.deleteCampaign(campaignId);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Campaign addCampaign(Campaign campaign) {
+        return campaignService.addCampaign(campaign);
+    }
+
+    @PUT
+    @Path("/{campaignId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Campaign updateCampaign(@PathParam(value = "campaignId") Long campaignId,
+                                   Campaign newCampaign) {
+        
+        newCampaign = campaignService.updateCampaign(newCampaign);
+        
+        return newCampaign;
+    }
+    
+}
