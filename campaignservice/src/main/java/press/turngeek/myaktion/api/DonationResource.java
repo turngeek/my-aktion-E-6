6f17b66ff3fb4e191c1cbf939b6bd0bb0cdf9e28
Martin Schmollinger <martin.schmollinger@gmail.com>
2020-02-05 16:21:36 +0100

    Added REST resource class for donation service

---
+++ campaignservice/src/main/java/press/turngeek/myaktion/api/DonationResource.java
@@ -0,0 +1,40 @@
+package press.turngeek.myaktion.api;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import press.turngeek.myaktion.boundary.DonationService;
+import press.turngeek.myaktion.model.Donation;
+
+/**
+ * DonationResource
+ */
+@Path("/donation")
+public class DonationResource {
+    @Inject
+    private DonationService donationService;
+
+    @GET
+    @Path("/list/{campaignId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Donation> getDonationList(@PathParam(value = "campaignId") Long campaignId) {
+        List<Donation> donations = donationService.getDonationList(campaignId);
+        return donations;
+    }
+
+    @POST
+    @Path("/{campaignId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addDonation(@PathParam(value = "campaignId")Long campaignId, Donation donation) {
+        donationService.addDonation(campaignId, donation);
+    }
+    
+}
